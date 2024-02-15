74cd8e1e99248606756d3c9bfc0da61fbc476156
TANJONIAINA Danie <tanjoniainadanie1309@gmail.com>
2024-02-15 12:34:00 +0300

    first commit

---
+++ src/main/java/com/spring/projet/InnovaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.spring.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/projet/InnovaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
